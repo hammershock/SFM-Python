956eda0e678c3622e58e0d80e832a85500a7acd8
hammershock <hammershock@163.com>
2024-05-09 23:42:33 +0800

    Added ImageDataset_SceauxCastle submodule

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+MultiView Structure From Motion
+         ---- A simple Practice
+
+Requirements:
+opencv-python
+numpy
+matplotlib
+
+@author: Hanmo Zhang
+@email: zhanghanmo@bupt.edu.cn
+"""
+
